1b25b15ae190b4e3d70a8dd87f36798e593ccf6d
Artem Humeniuk <102350389+GreegsD@users.noreply.github.com>
2023-11-23 14:29:43 +0200

    Add files via upload

--- Task_19.py
+++ Task_19.py
@@ -1,16 +1,17 @@
 import math
 
 
+def data(file_path='input.txt'):
+    with open(file_path, 'r') as file:
+        numbers = file.readline().split()
+        a = float(numbers[1])
+        z = float(numbers[0])
+        d = (1 / (z - 2*a)**2) - z**2
+        f = d ** (2 / 3)
+    return a, z, d, f
+result = data()
 
-file = open('input.txt', 'r')
-numbers = file.readline().split()
-a = float(numbers[1])
-z = float(numbers[0])
-d = (1 / ((z - 2 * a) ** 2)) - z ** 2
-f = d ** 2/3
-
-
-print(f"a = {a}\nz = {z}\nd = {d}\nf = {f}")
-
-x = open("output.txt", 'w')
-x.write(f"d = {d}\nf = {f}")
+result_str = f"x: {result[0]}\n n: {result[1]}\n m: {result[2]}\n f: {result[3]}"
+print(result_str)
+with open("output.txt", 'w') as f:
+    f.write(result_str)
